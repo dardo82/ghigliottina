1643e7a1bcc6f45b2fb34119b02c464aad901065
Michele Venturi <dardo82@gmail.com>
2014-07-05 22:09:57 +0200

    vocabolario fondamentale -> vocabolario di base

--- Back-End/vocabolario.sh
+++ Back-End/vocabolario.sh
@@ -1,21 +1,22 @@
 #!/bin/zsh
-P="parole"; V="vocabolario"; F="fondamentale"; VF="$V-$F.pdf"; VT="$V.txt"; rm -f $VT
-curl "http://www.vivante-pitagora.it/wp-content/uploads/2009/05/ALLEGATO-2.pdf" > $VF
-pdftotext -raw -enc UTF-8 $VF - | awk -v RS=" " 'NR>3{print l}{l=$0}' > $P.txt
+P="Parole"; V="Vocabolario"; B="Base"; VB="$V-$B.txt"; VT="${V:l}.txt"; rm -f $VT; mkdir $P
 FBU="https://www.googleapis.com/freebase/v1/mqlread/?lang=%2Flang%2Fit&query="
-WZU="https://it.wiktionary.org/w/index.php?action=raw&title="; mkdir $P
+VBU="http://materialefossati.altervista.org/download/${V}${B}Linkato.doc"
+WZU="http://it.wiktionary.org/w/index.php?action=raw&title="
+TCU="http://www.treccani.it/$V/"
+curl -s $VBU | textutil -stdin -stdout -convert txt > $VB
+cat $VB | egrep -v '\\|’|\.$' | tr ' ' '\n' | egrep -v '\(|\)' | egrep '^[A-Z]' > $P.txt
 for p in `cat $P.txt`; do
     n=$[$n+1]; p=${p:l}; PT="$P/$p.txt"
     if [ $n -lt 600 ]; then 
         WRK="b804a"; else
         WRK="12bee"
     fi
-    TCU="http://www.treccani.it/$V/$p/"
-    WRU="http://api.wordreference.com/$WRK/iten/$p"
+    WRU="http://api.wordreference.com/$WRK/iten/"
     FBQ=`urlenc -t "[{ \"name\": null, \"name~=\": \"$p\" }]"`
-    curl "$WRU" | egrep -io "W2\'>[^<>]+" >> $PT
     curl "$FBU$FBQ" | egrep -io ".*name.*" >> $PT
-    curl "$TCU" | egrep -io "m>[^<>]{3,}</e" >> $PT
+    curl "$WRU$p" | egrep -io "W2\'>[^<>]+" >> $PT
+    curl "$TCU$p" | egrep -io "m>[^<>]{3,}</e" >> $PT
     curl "$WZU$p" | egrep -io "^(#|:)?\*[^\*{:]+[^{:]+" >> $PT
 done
 for f in $P/*; do
@@ -23,4 +24,4 @@ for f in $P/*; do
     cat $f >> $VT
     echo "\n" >> $VT
 done
-rm -fr $P $P.txt $VF
+rm -fr $P $P.txt $VB
