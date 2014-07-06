c0cd5ed74fa9c61dbbad1def25dc3e7518c4daba
Michele Venturi <dardo82@gmail.com>
2014-07-06 03:04:30 +0200

    semplificato un po' lo script

--- Back-End/vocabolario.sh
+++ Back-End/vocabolario.sh
@@ -1,20 +1,17 @@
 #!/bin/zsh
-P="Parole"; V="Vocabolario"; B="Base"; VB="$V-$B.txt"; VT="${V:l}.txt"; rm -f $VT; mkdir $P
+P="Parole"; V="Vocabolario"; B="Base"; VB="$V-$B.txt"; VT="${V:l}.txt"
 FBU="https://www.googleapis.com/freebase/v1/mqlread/?lang=%2Flang%2Fit&query="
 VBU="http://materialefossati.altervista.org/download/${V}${B}Linkato.doc"
 WZU="http://it.wiktionary.org/w/index.php?action=raw&title="
+WRU="http://api.wordreference.com/12bee/iten/"
 TCU="http://www.treccani.it/$V/"
-curl -s $VBU | textutil -stdin -stdout -convert txt > $VB
-cat $VB | egrep -v '\\|’|\.$' | tr ' ' '\n' | egrep -v '\(|\)' | egrep '^[A-Z]' > $P.txt
+rm -f $VT; mkdir $P
+curl $VBU | textutil -stdin -stdout -convert txt > $VB
+cat $VB | egrep -v '\\|’|\.$' | tr ' ' '\n' | egrep '^[A-Z]' > $P.txt
 for p in `cat $P.txt`; do
-    n=$[$n+1]; p=${p:l}; PT="$P/$p.txt"
-    if [ $n -lt 600 ]; then 
-        WRK="b804a"; else
-        WRK="12bee"
-    fi
-    WRU="http://api.wordreference.com/$WRK/iten/"
-    FBQ=`urlenc -t "[{ \"name\": null, \"name~=\": \"$p\" }]"`
-    curl "$FBU$FBQ" | egrep -io ".*name.*" >> $PT
+    p=${p:l}; PT="$P/$p.txt"
+    Q=`urlenc -t "[{ \"name\": null, \"name~=\": \"$p\" }]"`
+    curl "$FBU$Q" | egrep -io ".*name.*" >> $PT
     curl "$WRU$p" | egrep -io "W2\'>[^<>]+" >> $PT
     curl "$TCU$p" | egrep -io "m>[^<>]{3,}</e" >> $PT
     curl "$WZU$p" | egrep -io "^(#|:)?\*[^\*{:]+[^{:]+" >> $PT
