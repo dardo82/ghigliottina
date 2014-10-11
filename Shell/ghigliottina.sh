15f750654fb2f523edf1f6a9b7af8007f634decd
Michele Venturi <dardo82@gmail.com>
2014-10-11 07:27:12 +0200

    fixed bugs

--- Shell/ghigliottina.sh
+++ Shell/ghigliottina.sh
@@ -1,48 +1,21 @@
 #!/bin/zsh
-
-export DICT="vocabolario.txt" SOL="soluzione.txt" TEMP="temporaneo.txt" HINTS="indizi.txt"
-
 function search {
- for arg in ${=*}; do
-  if [ $(awk -v RS="" 'END{print NR}' $DICT) -gt 1 ]; then
-   awk -v arg="$arg" -v RS="" -v ORS="\n\n" '{if ($0~arg) print $0}' $DICT > $SOL
-   cp $SOL $TEMP; DICT="$TEMP"
-  fi
- done
+awk -v RS="" "/${1// //&&/}/"'{sub(/.txt/,"");print $1}' $2
 }
-function hints {
- for arg in ${=*}; do
-  awk -v txt="$arg.txt" -v RS="" '{if ($0~txt) print $0}' $DICT >> $HINTS
- done
-}
-function solve {
- awk -v arg="$1" -v RS="" '{sub(/\.txt/,"");if ($0~arg) print $1}' $HINTS
-}
-function output {
-OUT="$OUT $(awk -v RS="" '{sub(/\.txt/,"");print $1}' $1)"
-}
-
-argv=${*:l}
-hints $argv
-search $argv
-if [ -s "$SOL" ]; then
- output $SOL
- echo ${OUT:1}
+eval LAST=\${$#}
+if [ "${LAST:e}" = "txt" ]; then
+    ARGV=${${*:l}/$LAST/}; DICT="$LAST"
 else
- TEST="1"
- for arg in "$argv"; do
-  search ${argv/$arg/}
-  if [ -s "$SOL" ]; then
-   output $SOL
-  fi
- done
+    ARGV=${*:l}; DICT="vocabolario.txt"
 fi
-if [ -n "$TEST" ]; then
- for arg in "$OUT"; do
-  if [ "$(echo $(solve $arg))" == "$argv" ]; then
-   echo ${arg:1}
-  fi
- done
+SOL=$(search $ARGV $DICT)
+if [ "$SOL" ]; then
+    echo "$SOL"
+else
+    for arg in ${=ARGV}; do
+        SOL=$(search ${ARGV/$arg/} $DICT)
+        if [ "$SOL" ]; then
+            echo "$SOL"
+        fi
+    done
 fi
-rm $SOL $TEMP $HINTS
-
--- Shell/vocabolario.sh
+++ Shell/vocabolario.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-P="parole"; V="vocabolario"; B="base"; PT="$P.txt"; VT="$V.txt"; VBT="$V$B.txt"
+P="parole"; V="vocabolario"; B="base"; PT="$P.txt"; VT="$V.txt"; VBT="$V-$B.txt"
 FBU="https://www.googleapis.com/freebase/v1/mqlread/?lang=%2Flang%2Fit&query="
 VBU="materialefossati.altervista.org/download/${(C)V}${(C)B}Linkato.doc"
 WZU="it.wiktionary.org/w/index.php?action=raw&title="
@@ -7,18 +7,18 @@ WRU="api.wordreference.com/12bee/json/iten"
 WAU="web.archive.org/web/20140812203035"
 TCU="www.treccani.it/$V/"
 rm -fr $P $VT; mkdir $P
-curl $VBU | textutil -stdin -stdout -convert txt > $VBT
+curl -s $VBU | textutil -stdin -stdout -convert txt > $VBT
 cat $VBT | egrep -v '\\|’|\.$' | tr " " \\n | egrep '^[A-Z]' > $PT
-for p in `cat $PT`; do
+for p in $(cat $PT); do
     p="${p:l}"; PPT="$P/$p.txt"; q=`urlenc -nt "[{ \"name\": null, \"name~=\": \"$p\" }]"`
-    curl -L $WAU/$WRU/$p/ | egrep -io '"[^"]*'"(( ?$p)|($p ?))"'[^"]*"' >> $PPT
-    curl $WZU$p | egrep -io '^(#|:)?\*[^\*{:]+[^{:]+' >> $PPT
-    curl $TCU$p/ | egrep -io 'm>[^<>]{3,}</e' >> $PPT
-    curl $FBU$q | egrep -io '.*name.*' >> $PPT
+    curl-s -L $WAU/$WRU/$p/ | egrep -io '"[^"]*'"(( ?$p)|($p ?))"'[^"]*"' >> $PPT
+    curl -s $WZU$p | egrep -io '^(#|:)?\*[^\*{:]+[^{:]+' >> $PPT
+    curl -s $TCU$p/ | egrep -io 'm>[^<>]{3,}</e' >> $PPT
+    curl -s $FBU$q | egrep -io '.*name.*' >> $PPT
 done
 for f in $P/*; do
     echo ${f##*/} >> $VT
     cat $f >> $VT
-    echo \\n >> $VT
+    echo >> $VT
 done
 rm -fr $P $PT $VBT
