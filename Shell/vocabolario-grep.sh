e01d770e81f23cfcb9adbad570e54eb5d05be5af
Michele Venturi <dardo82@gmail.com>
2014-10-14 09:20:17 +0200

    updated dictionary and removed test scripts

--- Shell/vocabolario-grep.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-LC_ALL="C"; PTD="parole-test"; PTT="parole-test.txt"
-for f in $PTD/*.txt; do for p in $(cat $PTT); do if [ "$(grep -F ${${f##*/}:r} $PTD/${p:l}.txt)" ]; then echo $p>>$f; fi; done; done 
