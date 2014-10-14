732209ab203374641332d7f9c6f7d60d42b0b207
Michele Venturi <dardo82@gmail.com>
2014-10-14 05:58:27 +0200

    little updates to dictionary script

--- Shell/test/vocabolario-test.awk
+++ Shell/test/vocabolario-test.awk
@@ -1,1 +1,1 @@
-BEGIN{wf="../parole-test.txt";while((getline l<wf)>0)s=(s RS l);split(s,wa);RS="";FS="."}{for(i in wa)if($0~wa[i])print $1>>(wa[i]".txt")}
+BEGIN{wf="parole.txt";while((getline l<wf)>0)s=(s RS l);split(s,wa);RS="";FS="."}{for(i in wa)if($0~wa[i])print $1>>("parole/"wa[i]"-awk.txt")}
--- Shell/vocabolario.sh
+++ Shell/vocabolario.sh
@@ -1,20 +1,20 @@
 #!/bin/zsh
 P="parole"; V="vocabolario"; B="base"; PT="$P.txt"; VT="$V.txt"; VBT="$V-$B.txt"
-FBU="https://www.googleapis.com/freebase/v1/mqlread/?lang=%2Flang%2Fit&query="
-VBU="materialefossati.altervista.org/download/${(C)V}${(C)B}Linkato.doc"
-WZU="it.wiktionary.org/w/index.php?action=raw&title="
-WRU="api.wordreference.com/12bee/json/iten"
-WAU="web.archive.org/web/20140812203035"
-TCU="www.treccani.it/$V/"
+FB="https://www.googleapis.com/freebase/v1/mqlread/?lang=%2Flang%2Fit&query="
+VB="materialefossati.altervista.org/download/${(C)V}${(C)B}Linkato.doc"
+WZ="it.wiktionary.org/w/index.php?action=raw&title="
+WR="api.wordreference.com/12bee/json/iten"
+WA="web.archive.org/web/20140812203035"
+TC="www.treccani.it/$V/"
 rm -fr $P $VT; mkdir $P
-curl -s $VBU | textutil -stdin -stdout -convert txt > $VBT
-cat $VBT | egrep -v '\\|’|\.$' | tr " " \\n | egrep '^[A-Z]' > $PT
+curl -s $VB | textutil -stdin -stdout -convert txt > $VBT
+cat $VBT | egrep -v '\\|’|\.$' | tr " " \\n | egrep '^[A-Z]' | tr [A-Z] [a-z] > $PT
 for p in $(cat $PT); do
-    p="${p:l}"; PPT="$P/$p.txt"; q=`urlenc -nt "[{ \"name\": null, \"name~=\": \"$p\" }]"`
-    curl-s -L $WAU/$WRU/$p/ | egrep -io '"[^"]*'"(( ?$p)|($p ?))"'[^"]*"' >> $PPT
-    curl -s $WZU$p | egrep -io '^(#|:)?\*[^\*{:]+[^{:]+' >> $PPT
-    curl -s $TCU$p/ | egrep -io 'm>[^<>]{3,}</e' >> $PPT
-    curl -s $FBU$q | egrep -io '.*name.*' >> $PPT
+    PPT="$P/$p.txt"; q=`urlenc -nt "[{ \"name\": null, \"name~=\": \"$p\" }]"`
+    curl -L $WA/$WR/$p/ | egrep -io '"[^"]*'"(( ?$p)|($p ?))"'[^"]*"' >> $PPT
+    curl $WZ$p | egrep -io '^(#|:)?\*[^\*{:]+[^{:]+' >> $PPT
+    curl $TC$p/ | egrep -io 'm>[^<>]{3,}</e' >> $PPT
+    curl $FB$q | egrep -io '.*name.*' >> $PPT
 done
 for f in $P/*; do
     echo ${f##*/} >> $VT
